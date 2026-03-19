d80d17b4f14b373016587ac472b127fffa145858
Irineu Marcelo MAIOLI <irineu_marcelo@outlook.com>
2026-03-19 17:38:14 -0300

    refactor(core): integrate chat interface into main pages and update proxy

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,16 +1,21 @@
 import { ThemeProvider } from "@/components/ThemeProvider"
-import '../globals.css';
+import '../../globals.css';
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+// Root layout requires suppressHudrationWaring when using next-themes
+export default function LocaleLayout({ children }: { children: ReactNode }) {
   return (
-    // suppressHydrationWarning is mandatory when using next-theme to prevent React mistmatch errors
-    <html lang="en" suppressContentEditableWarning>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Safely import Material Symbols to prevent rendering icons as plain text */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-gray-500 dark:bg-[#0f172a] transition-colors duration-300">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-        >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
         </ThemeProvider>
       </body>
--- src/proxy.ts
+++ src/proxy.ts
@@ -21,5 +21,5 @@ export default auth((req) => {
 
 export const config = {
     // Match all pathname except for API routes, static file and images
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+    matcher: ['/((?!api|_next/static|_next/image|logoHeader.png|logoIcontab.icon|system|favicon.ico).*)']
 }
