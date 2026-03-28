f2458d0005d40b9b855ee354eb2231f36c32f3fb
Irineu Marcelo Maioli <irineu_marcelo@outlook.com>
2026-03-28 01:12:58 -0300

    feat(i18n): modify language routing definitions and rules

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,10 @@
 import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
 
 // Defines the strict supported locales and the fallback
 export const routing = defineRouting({
     locales: ['pt-BR', 'en-US', 'es-LA'],
     defaultLocale: 'pt-BR'
 });
+
+export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
