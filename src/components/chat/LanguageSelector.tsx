f28c3f73cd05e616cfa112f759bae10c9cfd6705
Irineu Marcelo Maioli <irineu_marcelo@outlook.com>
2026-03-28 01:11:42 -0300

    feat(i18n): improve language selection component

--- src/components/chat/LanguageSelector.tsx
+++ src/components/chat/LanguageSelector.tsx
@@ -1,20 +1,45 @@
+import { useLocale } from 'next-intl';
+import { useRouter, usePathname } from '@/i18n/routing'
 import { LanguageButton } from './LanguageButton';
+import { BR, US, ES } from 'country-flag-icons/react/3x2'
+import { useTransition } from 'react';
 
 export function LanguageSelector() {
+    const currentLocale = useLocale();
+    const router = useRouter();
+    const pathname = usePathname();
+    const [isPending, startTransition] = useTransition();
+
+    const handleLanguageChange = (newLocale: string) => {
+        if (currentLocale !== newLocale) {
+            startTransition(() => {
+                router.replace(pathname, { locale: newLocale })
+            })
+        }
+    };
+
     return (
-        <div className="flex items-center gap-2 pr-3 border-r border-gray-200 dark:border-[#283039] transition-colors">
-            <LanguageButton
-                title="English"
-                flagUrl="https://flagcdn.com/w80/us.png"
-            />
+        <div className="flex items-center gap-2 pr-3 border-r border-gray-200 dark:border-custom_surface transition-colors">
             <LanguageButton
                 title="Português"
-                flagUrl="https://flagcdn.com/w80/br.png"
-                isActive={true}
+                icon={BR}
+                isShowTitle={false}
+                isActive={currentLocale === 'pt-BR'}
+                onClick={() => handleLanguageChange('pt-BR')}
+            />
+            <LanguageButton
+                title="English"
+                isShowTitle={false}
+                icon={US}
+                isActive={currentLocale === 'en-US'}
+                onClick={() => handleLanguageChange('en-US')}
             />
             <LanguageButton
                 title="Español"
-                flagUrl="https://flagcdn.com/w80/es.png"
+                isShowTitle={false}
+                icon={ES}
+                isActive={currentLocale === 'es-LA'}
+                onClick={() => handleLanguageChange('es-LA')}
             />
         </div>
     );
